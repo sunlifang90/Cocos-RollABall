23d02de7e0f4b6593ba2ce555047199a2fc3f54f
sunlifang1990 <982290835@qq.com>
2025-12-17 09:47:31 +0800

    fix:完善Player移动功能

--- assets/Scripts/Player.ts
+++ assets/Scripts/Player.ts
@@ -1,8 +1,13 @@
-import { _decorator, Component, EventKeyboard, Input, input, KeyCode, Node } from 'cc';
+import { _decorator, Component, EventKeyboard, Input, input, KeyCode, Node, Vec2 } from 'cc';
 const { ccclass, property } = _decorator;
 
 @ccclass('Player')
 export class Player extends Component {
+
+    @property
+    private speed: number = 5;
+    private movDir: Vec2 = Vec2.ZERO
+
     start() {
         console.log('Player script started.');
         input.on(Input.EventType.KEY_DOWN, this.onKeyDown, this);
@@ -11,7 +16,8 @@ export class Player extends Component {
     }
 
     update(deltaTime: number) {
-        
+         const currentPos = this.node.position;
+         this.node.setPosition(currentPos.x + this.movDir.x * this.speed * deltaTime, currentPos.y, currentPos.z + this.movDir.y * this.speed * deltaTime);
     }
 
     protected onDestroy(): void {
@@ -20,29 +26,57 @@ export class Player extends Component {
         input.off(Input.EventType.KEY_PRESSING, this.onKeyPressing, this);
     }
 
+    /**
+     * 设置移动方向
+     * @param event 
+     */
     private onKeyDown(event: EventKeyboard) {
-        console.log('Key Down: ' + event.keyCode);
+        switch (event.keyCode) {
+            case KeyCode.KEY_A:
+                //设置左移
+                this.movDir = new Vec2(-1, this.movDir.y);
+                break;
+            case KeyCode.KEY_D:
+                //设置右移
+                this.movDir = new Vec2(1, this.movDir.y);
+                break;
+            case KeyCode.KEY_W:
+                //设置前进
+                this.movDir = new Vec2(this.movDir.x, -1);
+                break;
+            case KeyCode.KEY_S:
+                //设置后退
+                this.movDir = new Vec2(this.movDir.x, 1);
+                break;
+        }
     }
 
+    /**
+     * 清空移动方向
+     * @param event 
+     */
     private onKeyUp(event: EventKeyboard) {
-        console.log('Key Up: ' + event.keyCode);
+                switch (event.keyCode) {
+            case KeyCode.KEY_A:
+                //停止左移
+                this.movDir.x = 0;
+                break;
+            case KeyCode.KEY_D:
+                //停止右移
+                this.movDir.x = 0;
+                break;
+            case KeyCode.KEY_W:
+                //停止前进
+                this.movDir.y = 0;
+                break;
+            case KeyCode.KEY_S:
+                //停止后退
+                this.movDir.y = 0;
+                break;
+        }
     }
 
     private onKeyPressing(event: EventKeyboard) {
-        const currentPos = this.node.position;
-        switch (event.keyCode) {
-            case KeyCode.KEY_A:
-                this.node.setPosition(currentPos.x - 0.2, currentPos.y, currentPos.z);
-                break;
-            case KeyCode.KEY_D:
-                this.node.setPosition(currentPos.x + 0.2, currentPos.y, currentPos.z);
-                break;
-            case KeyCode.KEY_W:
-                this.node.setPosition(currentPos.x, currentPos.y, currentPos.z - 0.2);
-                break;
-            case KeyCode.KEY_S:
-                this.node.setPosition(currentPos.x, currentPos.y, currentPos.z + 0.2);
-                break;
-        }
+
     }
 }
